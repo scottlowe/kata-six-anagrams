9f4b0d92879fb9c5c09103f085f5035fc9ede54d
Scott Lowe <scottwithtwotees@gmail.com>
2012-03-06 17:06:59 +1100

    Deal with cases where first second word contains all the letters in the first word and vice versa. Word comparison check also improves performance.

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -1,5 +1,7 @@
 class AnagramFinder
   def is_anagram?(first, second)
+    return if first.length != second.length
+    
     first_chars  = first.chars.select {|c| c}
     second_chars = second.chars.select {|c| c}
     first_chars.all? {|char| second_chars.include? char }
--- spec/anagram_finder_spec.rb
+++ spec/anagram_finder_spec.rb
@@ -3,13 +3,38 @@ require 'spec_helper'
 describe AnagramFinder do
   describe "#is_anagram?" do
     context "when two words are the same length" do
-
       it "should return true when the words are identical" do
         subject.is_anagram?("hello", "hello").should be_true
       end
 
-      it "should return true when the words not identical" do
+      it "should return true when the words contain the same characters in a different order" do
+        subject.is_anagram?("hello", "elolh").should be_true
+      end
+
+      it "should return false when the words contain some different characters" do
         subject.is_anagram?("hello", "booth").should be_false
+      end
+
+      it "should return false when the words all characters are different" do
+        subject.is_anagram?("hello", "xgpbt").should be_false
+      end
+    end
+
+    context "when two words are a different length" do
+      it "should return false when the words are nearly the same, but for an extra last character on the second word" do
+        subject.is_anagram?("hello", "hellox").should be_false
+      end
+
+      it "should return false when the words are nearly the same, but for an extra first character on the second word" do
+        subject.is_anagram?("hello", "xhello").should be_false
+      end
+
+      it "should return false when the words are nearly the same, but for an extra first character on the first word" do
+        subject.is_anagram?("xhello", "hello").should be_false
+      end
+
+      it "should return false when the words are nearly the same, but for an extra second character on the first word" do
+        subject.is_anagram?("hellox", "hello").should be_false
       end
     end
   end
